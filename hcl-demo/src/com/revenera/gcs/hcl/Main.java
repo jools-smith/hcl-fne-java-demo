f204fe7eed31276dc2874e12b0cb6c60ae6b215d
Jools <julian.smith@flexera.com>
2025-01-21 15:43:27 +0000

    IO changes

--- hcl-demo/src/com/revenera/gcs/hcl/Main.java
+++ hcl-demo/src/com/revenera/gcs/hcl/Main.java
@@ -1,20 +1,97 @@
 package com.revenera.gcs.hcl;
 
+import com.flexnet.licensing.client.ICapabilityResponseData;
+import com.flexnet.licensing.client.IFeature;
+import com.flexnet.licensing.client.ILicense;
+import com.flexnet.licensing.client.IResponseStatus;
+import com.flexnet.lm.FlxException;
 import com.revenera.gcs.hcl.fne.Client;
 import com.revenera.gcs.hcl.fne.ClientFactory;
 
+import java.net.URI;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+class IO {
+  static void header(final String header) {
+    System.out.println(header);
+    System.out.println(header.chars().mapToObj(i -> "-").collect(Collectors.joining()));
+  }
+
+  static String concatenate(final Object...parts) {
+    return Arrays.stream(parts).map(Object::toString).collect(Collectors.joining(" | "));
+  }
+
+  static void printFeatureCorrection(final String caption, final List<IFeature> features) {
+
+    header(caption);
+
+    System.out.println("count = " + features.size());
+    for (final IFeature feature : features) {
+      System.out.println(concatenate(feature.getName(),
+                                     feature.getVersion(),
+                                     feature.getCount(),
+                                     feature.getStartDate(),
+                                     feature.getExpiration(),
+                                     feature.getAcquisitionStatus(),
+                                     feature.getAvailableAcquisitionCount()));
+    }
+    System.out.println();
+  }
+
+  static void printLicenseCollection(final String caption, final List<ILicense> licenses) {
+
+    header(caption);
+    System.out.println("count = " + licenses.size());
+    for (final ILicense license : licenses) {
+      System.out.println(concatenate(license.getName(),
+                                     license.getVersion(),
+                                     license.getCount(),
+                                     license.getStartDate(),
+                                     license.getExpiration()));
+    }
+    System.out.println();
+  }
+
+  static void printResponseDetails(final ICapabilityResponseData response) throws FlxException {
+
+    System.out.println("Capability Response Details");
+    System.out.println("---------------------------");
+
+    final List<IFeature> features = response.getFeatures();
+    System.out.println("feature count = " + features.size());
+    for (final IFeature feature : features) {
+      System.out.println(concatenate(feature.getName(),
+                                     feature.getVersion(),
+                                     feature.getCount(),
+                                     feature.getStartDate(),
+                                     feature.getExpiration()));
+    }
+
+    final List<IResponseStatus> statuses = response.getResponseStatus();
+    System.out.println("response status count = " + statuses.size());
+    for (final IResponseStatus status : statuses) {
+      status.getDetails();
+      System.out.println(concatenate(status.getCode(), status.getStatus(), status.getCategory(), status.getDetails()));
+    }
+    System.out.println();
+  }
+}
 public class Main {
+
+
 
   public static void main(final String...args) {
     try {
       System.out.println("hello...");
 
-      final String url = "https://flex1113-uat.compliance.flexnetoperations.com/deviceservices";
+      final URI url = Client.deviceUri("flex1113-uat", "com");
 
       final Path storage = Paths.get("d:", "gcs", "hcl-fne-java-demo", "storage");
+
       final Client client = ClientFactory
               .createFactory()
               .withIdentity(IdentityClient.IDENTITY_DATA)
@@ -24,11 +101,26 @@ public class Main {
               .withHostType("FLX_CLIENT")
               .initializeClient();
 
-      client.callHome(url, "6ba6-6083-276d-4406-9296-981f-1cd0-e888");
+      IO.printFeatureCorrection("Features in Trusted Storage", client.manager().getFeaturesFromTrustedStorage(false));
 
-      client.manager().getFeaturesFromTrustedStorage(true).forEach(feature -> {
-        System.out.println(feature.getName() + " | " + feature.getVersion()+ " | " + feature.getCount() +  " | " + feature.getStartDate() + " | " + feature.getExpiration());
-      });
+      final ICapabilityResponseData response = client.callHome(url, "6ba6-6083-276d-4406-9296-981f-1cd0-e888");
+
+      IO.printResponseDetails(response);
+
+      final List<IFeature> features = client.manager().getFeaturesFromTrustedStorage(false);
+      IO.printFeatureCorrection("Features in Trusted Storage", features);
+
+      // acquire all features
+      for (final IFeature feature : features) {
+        client.acquire(feature.getName(), feature.getVersion(), 1);
+      }
+      IO.printLicenseCollection("Acquired Licenses", client.manager().getLicenses());
+
+      // return all licenses
+      for (final IFeature feature : features) {
+        client.release(feature.getName());
+      }
+      IO.printLicenseCollection("Acquired Licenses", client.manager().getLicenses());
     }
     catch (final Throwable t) {
       t.printStackTrace(System.err);
--- hcl-demo/src/com/revenera/gcs/hcl/fne/Client.java
+++ hcl-demo/src/com/revenera/gcs/hcl/fne/Client.java
@@ -1,31 +1,56 @@
 package com.revenera.gcs.hcl.fne;
 
-import com.flexnet.licensing.client.ICapabilityRequestOptions;
-import com.flexnet.licensing.client.ICapabilityResponseData;
-import com.flexnet.licensing.client.ILicenseManager;
-import com.flexnet.licensing.client.ILicensing;
+import com.flexnet.licensing.client.*;
 import com.flexnet.lm.FlxException;
 import com.flexnet.lm.net.Comm;
 
+import java.net.URI;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Client {
   final ILicensing licensing;
 
 
+  public static URI deviceUri(final String tenant, final String domain) {
+    return URI.create(String.format("https://%s.compliance.flexnetoperations.%s/deviceservices", tenant, domain));
+  }
+
+  public static URI serverUri(final String tenant, final String domain, final String instanceId) {
+    return URI.create(String.format("https://%s.compliance.flexnetoperations.%s/instances/%s/request", tenant, domain, instanceId));
+  }
+
   Client(final ILicensing licensing) {
     this.licensing = licensing;
   }
+
 
   public ILicenseManager manager() {
     return this.licensing.getLicenseManager();
   }
 
-  public void callHome(final String uri, final String...rights) throws FlxException {
+  public ILicense acquire(final String feature, final String version, final long count) throws FlxException {
+    return manager().acquire(feature, version, count);
+  }
 
-    final ILicenseManager manager = manager();
+  public void release(final ILicense license) throws FlxException {
+    manager().releaseLicense(license);
+  }
 
-    final ICapabilityRequestOptions options = manager.createCapabilityRequestOptions();
+  public void release(final String feature) throws FlxException {
+     for(final ILicense license : new ArrayList<>(manager().getLicenses(feature)))    {
+       release(license);
+     }
+  }
+
+  public List<ILicense> getLicenses() {
+    return manager().getLicenses();
+  }
+
+  public ICapabilityResponseData callHome(final URI uri, final String...rights) throws FlxException {
+
+    final ICapabilityRequestOptions options = manager().createCapabilityRequestOptions();
 
     options.forceResponse();
 
@@ -33,12 +58,11 @@ public class Client {
       options.addRightsId(right, 1);
     });
 
-    final byte[] request = manager.generateCapabilityRequest(options);
+    final byte[] request = manager().generateCapabilityRequest(options);
 
-    final byte[] response = Comm.getHttpInstance(uri).sendBinaryMessage(request);
+    final byte[] response = Comm.getHttpInstance(uri.toString()).sendBinaryMessage(request);
 
-    final ICapabilityResponseData capabilityResponse = manager.processCapabilityResponse(response);
-
+    return manager().processCapabilityResponse(response);
   }
 
 }
